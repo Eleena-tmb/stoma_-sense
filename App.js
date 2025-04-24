a6e50302ce369880088dd26091ae4ab6973e3adc
Eleena <alinataym41@gmail.com>
2025-04-24 16:49:51 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome Back!</h1>
+      <p>Login to access your plant tracker</p>
+    </div>
+  );
+}
+
+export default App;
